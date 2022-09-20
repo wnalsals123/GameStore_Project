a9ae9d4664195bdaf1441c32920a58ee5fc1569a
JU MIN JONG <wnalsals12@naver.com>
2022-09-21 01:50:32 +0900

    결제 완료 시 오류 수정 및 데이터 초기화

--- gamestore/src/home/App.js
+++ gamestore/src/home/App.js
@@ -10,7 +10,7 @@ import { getCookie } from '../function/Cookie';
 
 function App() {
   const location = useLocation()
-  const version = '2.7'
+  const version = '3.0'
   const [cart, setCart] = useState(0)
   const [sideIsOpen, setSideIsOpen] = useState(false)
   const [isAddCart, setIsAddCart] = useState(false)
